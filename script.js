e12b58ab1e83ee61d5c00b2e2b77f1b2e77911b6
DDkhoa2k8 <khoaddtd01140@gmail.com>
2025-05-18 13:44:37 +0700

    seses

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const svLink = "https://gchsedjronrenjwafuyb.supabase.co";
 const ap = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdjaHNlZGpyb25yZW5qd2FmdXliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc1NDgwNDQsImV4cCI6MjA2MzEyNDA0NH0.O76q9gHvB6frsGQNdDoNrfHOOox9WDh5kxa4AORNwKw";
-
+const reqTest = "https://gchsedjronrenjwafuyb.supabase.co/functions/v1/hyper-handler";
